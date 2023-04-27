4c5077ea0ad5ae9847b682ced1033c17d34b9d25
Bruno Drumond <93215985+ItsDrumond@users.noreply.github.com>
2023-04-27 15:29:55 +0100

    updated lab 5

--- lab5/video.c
+++ lab5/video.c
@@ -1,7 +1,10 @@
 #include <lcom/lcf.h>
+#include "video.h"
+
+vbe_mode_info_t info;
+void *video_mem;
 
 void *(vg_init)(uint16_t mode){
-    vbe_mode_info_t info;
     vbe_get_mode_info(mode, &info);
     reg86_t r86;
     memset(&r86, 0, sizeof(r86));
@@ -13,10 +16,6 @@ void *(vg_init)(uint16_t mode){
         printf("set_vbe_mode: sys_int86() failed \n");
         return NULL;
     }
-
-void *video_mem;         /* frame-buffer VM address 
-				   (static global variable*/ 
-
 
 struct minix_mem_range mr;
 unsigned int vram_base;  /* VRAM's physical addresss */
@@ -47,6 +46,34 @@ video_mem = vm_map_phys(SELF, (void *)mr.mr_base, vram_size);
 if(video_mem == MAP_FAILED){
    panic("couldn't map video memory");
 }
-    memset(video_mem, 0x35, vram_size);
+    memset(video_mem, 0, vram_size);
     return video_mem;
+}
+
+int (vg_draw_hline)(uint16_t x, uint16_t y, uint16_t len, uint32_t color){
+    uint8_t bytes = (info.BitsPerPixel+7) / 8;
+    uint8_t* ptr = (u_int8_t*) video_mem + (y + info.XResolution + x) * bytes;
+    for(u_int16_t i = 0; i < len ; i++){
+        for(uint8_t j = 0; j < bytes; j++){
+            *ptr = color >> j * 8;
+            ptr++;
+        }
+    }
+    return 0;
+}
+
+int (vg_draw_rectangle)(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color){
+    if(x > info.XResolution || y > info.YResolution){
+        return 1;
+    }
+    if((x + width) > info.XResolution){
+        width = info.XResolution - x; // o que falta desenhar
+    }
+    if((y + height) > info.YResolution){
+        height = info.YResolution - y; // o que falta desenhar
+    }
+    for (u_int16_t i = 0; i < height; i++){
+        vg_draw_hline(x, i+y, width, color);
+    }
+    return 0;
 }
---
+++ lab5/video.h
@@ -0,0 +1,11 @@
+#ifndef _LCOM_VIDEO_H
+#define _LCOM_VIDEO_H
+
+#include <lcom/lcf.h>
+
+void *(vg_init)(uint16_t mode);
+int (vg_draw_hline)(uint16_t x, uint16_t y, uint16_t len, uint32_t color);
+int (vg_draw_rectangle)(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color);
+
+
+#endif
